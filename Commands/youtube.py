e04d904f3eb00eeb1fd2a44a317620e54b9923bb
koolguru <koolguru2@gmail.com>
2020-04-11 18:11:02 -0700

    Add Youtube

---
+++ Commands/youtube.py
@@ -0,0 +1,23 @@
+from command import Command
+
+class Google(Command):
+    
+    def __init__(self):
+        super()
+        return
+    
+    def get_response(self, arg=None):
+        if arg:
+            return 'http://www.youtube.com/results?search_query={0}&search_type=&aq=-1&oq='.format(arg) 
+        else:
+            return 'http://www.youtube.com'
+
+    def get_keywords(self):
+        return [
+            "yt",
+            "youtube",
+            "tube"
+        ]
+
+    def get_description(self):
+        return "Searches Youtube"
